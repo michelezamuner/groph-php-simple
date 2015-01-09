47e8fc26aed65756fb4901247d6a4ec1878d3032
Michele Zamuner <michelezamuner@gmail.com>
2015-01-09 08:38:28 +0100

    Canceled local configuration mechanism sine it doesn't work. The problem is that when checking out branches the configuration-local.php is kept since it's ignored, and so it affects every branch.

--- configuration.php
+++ configuration.php
@@ -1,7 +1,4 @@
 <?php
-if (file_exists('configuration-local.php'))
-	return include 'configuration-local.php';
-else
-	return array(
-		'db' => 'groph.sqlite'
-	);
+return array(
+	'db' => 'groph.sqlite'
+);
