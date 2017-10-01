0bf6124204cb0e7f512d8ecac9b3a94e26d67b87
Michele Zamuner <michelezamuner@gmail.com>
2017-10-01 17:10:18 +0200

    Updated for OpenShift Online 3

--- provision.sh
+++ provision.sh
@@ -11,7 +11,7 @@ sed -i 's/error_reporting = E_ALL & ~E_DEPRECATED/error_reporting = E_ALL | E_ST
 sed -i 's/display_errors = Off/display_errors = On/g' /etc/php5/apache2/php.ini
 sed -i 's/display_startup_errors = Off/display_startup_errors = On/g' /etc/php5/apache2/php.ini
 sed -i 's/html_errors = Off/html_errors = On/g' /etc/php5/apache2/php.ini
-sed -i 's/disable_functions = */disable_functions = system, show_source, exec, shell_exec, proc_open, passthru, set_time_limit, ini_restore, mysql_list_dbs, ini_alter, dl, pfsockopen, openlog, syslog, symlink, link, chgrp, leak, popen, escapeshellcmd, apache_child_terminate, apache_get_modules, apache_get_version, apache_getenv, apache_note, apache_setenv, virtual, mb_send_mail/g' /etc/php5/apache2/php.ini
-sed -i 's/;open_basedir = */open_basedir = \/vagrant:\/usr\/lib\/php5:\/tmp/g' /etc/php5/apache2/php.ini
 
 a2dissite default && a2ensite default && service apache2 restart
+mkdir -p /usr/share/data/groph
+chown -R vagrant:vagrant /usr/share/data/groph
